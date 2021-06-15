93a63a2c33c40ff305d3a698bf5ff86d6414cac0
ohad maislish <ohad1282@gmail.com>
2021-06-15 14:39:55 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,24 +14,50 @@ resource "random_id" "id4" {
   byte_length = 8
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "s3-website-test.hashicorp.com"
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+
+provider "random" {}
+
+resource "random_string" "random" {
+  length = "16"
+  special = "false"
+  min_lower = "16"
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "env0-s3-${random_string.random.result}"
   acl    = "public-read"
-  policy = file("policy.json")
+  force_destroy = true
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = "${aws_s3_bucket.website_bucket.id}"
 
-    routing_rules = <<EOF
-[{
-    "Condition": {
-        "KeyPrefixEquals": "docs/"
-    },
-    "Redirect": {
-        "ReplaceKeyPrefixWith": "documents/"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "Public-Access",
+  "Statement": [
+    {
+      "Sid": "Allow-Public-Access-To-Bucket",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": [
+          "arn:aws:s3:::${aws_s3_bucket.website_bucket.bucket}/*"
+      ]
     }
-}]
-EOF
-  }
+  ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = "${aws_s3_bucket.website_bucket.bucket}"
+  key    = "index.html"
+  source = "../index.html"
+  content_type = "text/html"
 }
